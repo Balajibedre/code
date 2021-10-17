5342dceddab9d839c4f04f646618d42132fd857c
BALAJI BEDRE <89333765+Balajibedre@users.noreply.github.com>
2021-10-17 12:48:43 +0530

    Add files via upload

---
+++ know.py
@@ -0,0 +1,66 @@
+# /  -- SLASH
+
+# ;  -- SEMICOLON
+
+# ()  -- PRAENTHESES
+
+# .  -- FULL STOP 
+
+# _ -- HYPHER
+
+# ""  -- QUOTATION MARK
+
+# , -- COMMA
+
+# ...  -- EllIPSIS 
+
+# %  -- PERCENT
+
+# :  -- COLON 
+
+# ?  -- QUESTION MARK
+
+# !  -- EXCLAMATION MARK
+
+# &  -- AND
+
+# '  -- APOSTROPHE
+
+# >  -- GRESTER THAN 
+
+# <  -- LESS THAN
+
+# {}  -- CURLY BRACKETS
+
+# []  -- SQUARE BRACKET
+
+# =  -- EQUAL 
+
+# # -- HASH
+
+# __  -- UNDERSCORE
+
+# |  -- BAR
+
+# @   -- AT THE RATE SIGN 
+
+# .  -- BULLET
+
+# *  -- ASTERISK
+
+# \  -- BACKSLASH
+
+# / -- DIVISION  
+
+# ^  -- CIRCUMFLEX
+
+# << >> -- GUILLEMENT 
+
+# ~  -- ALMOST EQUAL TO 
+
+# *  -- MULTIPLICATON SING  
+
+# +,-  -- PLUS-MINUS SIGN
+
+
+
